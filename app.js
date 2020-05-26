b2b2b32a0627d77a47d0e38d93a47ec049c74ab7
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-05-26 23:41:37 +0530

    debugged server

--- app.js
+++ app.js
@@ -4,23 +4,27 @@ const app = express();
 const path = require('path');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const ejs = require('ejs');
+
+const employeeRoutes = require('./routes/employees');
 
 dotenv.config({path: './config.env'});
 
 //connecting to the database
-mongoose.connect(process.env.DATABASE.LOCAL, {
+mongoose.connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 });
 
-app.set('view', path.join(__dirname, 'view'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+app.use(employeeRoutes);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4242;
 app.listen(port, ()=>{
     console.log(`Server has started on port: ${port}`);
 })
